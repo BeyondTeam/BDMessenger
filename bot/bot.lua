9945691095874cf509b8d2bb8397735ff24f9d5a
Makan <makan.j5050@gmail.com>
2017-04-19 02:24:00 +0430

    Beyond Messenger V1

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,1 +1,232 @@
+package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
+package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
+    bot_token = "Token" --Put You Token Here
+   send_api = "https://api.telegram.org/bot"..bot_token
+sudo_id = 157059515
+require('./bot/methods')
+http = require('socket.http')
+https = require('ssl.https')
+url = require('socket.url')
+redis = (loadfile "./libs/redis.lua")()
+JSON = (loadfile "./libs/dkjson.lua")()
+serpent = (loadfile "./libs/serpent.lua")()
+function bot_run()
+	bot = nil
+	while not bot do
+		bot = send_req(send_api.."/getMe")
+	end
+	bot = bot.result
+	local runlog = bot.first_name.." [@"..bot.username.."]\nis run in: "..os.date("%F - %H:%M:%S")
+	print(runlog)
+	send_msg(sudo_id, runlog)
+	last_update = last_update or 0
+	last_cron = last_cron or os.time()
+	startbot = true
+end
 
+function send_req(url)
+	local dat, res = https.request(url)
+	local tab = JSON.decode(dat)
+	if res ~= 200 then return false end
+	if not tab.ok then return false end
+	return tab
+end
+
+function bot_updates(offset)
+	local url = send_api.."/getUpdates?timeout=10"
+	if offset then
+		url = url.."&offset="..offset
+	end
+	return send_req(url)
+end
+
+function load_data(filename)
+	local f = io.open(filename)
+	if not f then
+		return {}
+	end
+	local s = f:read('*all')
+	f:close()
+	local data = JSON.decode(s)
+	return data
+end
+
+function save_data(filename, data)
+	local s = JSON.encode(data)
+	local f = io.open(filename, 'w')
+	f:write(s)
+	f:close()
+end
+
+function msg_valid(msg)
+	local msg_time = os.time() - 20
+	if msg.date < tonumber(msg_time) then
+		print('\27[36m》》OLD MESSAGE《《\27[39m')
+		return false
+	end
+    return true
+end
+
+-- Returns a table with matches or nil
+function match_pattern(pattern, text, lower_case)
+  if text then
+    local matches = {}
+    if lower_case then
+      matches = { string.match(text:lower(), pattern) }
+    else
+      matches = { string.match(text, pattern) }
+    end
+      if next(matches) then
+        return matches
+      end
+  end
+  -- nil
+end
+  plugins = {}
+function match_plugins(msg)
+  for name, plugin in pairs(plugins) do
+    match_plugin(plugin, name, msg)
+  end
+end
+
+function match_plugin(plugin, plugin_name, msg)
+
+  -- Go over patterns. If one matches it's enough.
+if plugin.pre_process then
+        --If plugin is for privileged users only
+		local result = plugin.pre_process(msg)
+		if result then
+			print("pre process: ", plugin_name)
+		end
+	end
+  for k, pattern in pairs(plugin.patterns) do
+    local matches = match_pattern(pattern, msg.text)
+    if matches then
+      print("msg matches: ", pattern)
+      -- Function exists
+      if plugin.run then
+        -- If plugin is for privileged users only
+          local result = plugin.run(msg, matches)
+          if result then
+            send_msg(msg.chat.id, result, 'markdown')
+        end
+      end
+      -- One patterns matches
+      return
+    end
+  end
+end
+
+-- Save the content of _config to config.lua
+function save_config( )
+  serialize_to_file(_config, './data/config.lua')
+  print ('saved config into ./data/config.lua')
+end
+
+-- Create a basic config.json file and saves it.
+function create_config( )
+  -- A simple config with basic plugins and ourselves as privileged user
+  config = {
+    enabled_plugins = {
+    "core"
+    },
+    sudo_users = {157059515},--Sudo users
+    info_text = [[》Beyond Messenger V1.0
+An messenger bot based on plugin
+
+》https://github.com/BeyondTeam/BeyondMessenger
+
+》Admins :
+》@SoLiD ➣ Founder & Developer《
+》@Makan ➣ Developer & Sponser《
+》@ToOfan ➣ Developer《
+》@CliFather ➣ Developer《
+
+》Special thanks to :
+》@MrHalix
+》And Beyond Team Members
+
+》Our channel :
+》@BeyondTeam《
+
+》Our website :
+》http://BeyondTeam.ir
+]],
+  }
+  serialize_to_file(config, './data/config.lua')
+  print('saved config into ./data/config.lua')
+end
+
+-- Returns the config from config.lua file.
+-- If file doesn't exist, create it.
+function load_config( )
+  local f = io.open('./data/config.lua', "r")
+  -- If config.lua doesn't exist
+  if not f then
+    print ("Created new config file: data/config.lua")
+    create_config()
+  else
+    f:close()
+  end
+  local config = loadfile ("./data/config.lua")()
+  for v,user in pairs(config.sudo_users) do
+    print("Sudo user: " .. user)
+  end
+  return config
+end
+_config = load_config( )
+
+-- Enable plugins in config.json
+function load_plugins()
+  for k, v in pairs(_config.enabled_plugins) do
+    print("Loading plugin", v)
+
+    local ok, err =  pcall(function()
+      local t = loadfile("plugins/"..v..'.lua')()
+      plugins[v] = t
+    end)
+
+    if not ok then
+      print('\27[31mError loading plugin '..v..'\27[39m')
+	  print(tostring(io.popen("lua plugins/"..v..".lua"):read('*all')))
+      print('\27[31m'..err..'\27[39m')
+    end
+
+  end
+end
+
+bot_run()
+load_plugins()
+while startbot do
+	local res = bot_updates(last_update+1)
+	if res then
+		for i,v in ipairs(res.result) do
+			last_update = v.update_id
+			if v.edited_message then
+          if msg_valid(v.edited_message) then
+				match_plugins(v.edited_message)
+           end
+			elseif v.message then
+          if msg_valid(v.message) then
+				match_plugins(v.message)
+          end
+			end
+		end
+	else
+		print("error while")
+	end
+	if last_cron < os.time() - 30 then
+  for name, plugin in pairs(plugins) do
+		if plugin.cron then
+			local res, err = pcall(
+				function()
+					plugin.cron()
+				end
+        
+			)
+      end
+			if not res then print('error: '..err) end
+		end
+		last_cron = os.time()
+	end
+end
---
+++ bot/methods.lua
@@ -0,0 +1,97 @@
+function send_msg(chat_id, text, use_markdown)
+	local text_len = string.len(text)
+	local num_msg = math.ceil(text_len / 4000)
+	if num_msg <= 1 then
+		local send = send_api.."/sendMessage?chat_id="..chat_id.."&text="..url.escape(text)
+		if use_markdown then
+			send = send.."&parse_mode=Markdown"
+		end
+		return send_req(send)
+	else
+		text = text:gsub("*","")
+		text = text:gsub("`","")
+		text = text:gsub("_","")
+		local f = io.open("large_msg.txt", 'w')
+		f:write(text)
+		f:close()
+		local send = send_api.."/sendDocument"
+		local curl_command = 'curl -s "'..send..'" -F "chat_id='..chat_id..'" -F "document=@large_msg.txt"'
+		return io.popen(curl_command):read("*all")
+	end
+end
+
+function send_document(chat_id, name)
+	local send = send_api.."/sendDocument"
+	local curl_command = 'curl -s "'..send..'" -F "chat_id='..chat_id..'" -F "document=@'..name..'"'
+	return io.popen(curl_command):read("*all")
+end
+
+function forwardMessage(chat_id, from_chat_id, message_id)
+
+	local url = send_api .. '/forwardMessage?chat_id=' .. chat_id .. '&from_chat_id=' .. from_chat_id .. '&message_id=' .. message_id
+
+	return send_req(url)
+
+end
+
+function send_key(chat_id, text, keyboard, resize, mark)
+	local response = {}
+	response.keyboard = keyboard
+	response.resize_keyboard = resize
+	response.one_time_keyboard = false
+	response.selective = false
+	local responseString = JSON.encode(response)
+	if mark then
+		sended = send_api.."/sendMessage?chat_id="..chat_id.."&text="..url.escape(text).."&disable_web_page_preview=true&reply_markup="..url.escape(responseString)
+	else
+		sended = send_api.."/sendMessage?chat_id="..chat_id.."&text="..url.escape(text).."&parse_mode=Markdown&disable_web_page_preview=true&reply_markup="..url.escape(responseString)
+	end
+	return send_req(sended)
+end
+
+function string:input()
+	if not self:find(' ') then
+		return false
+	end
+	return self:sub(self:find(' ')+1)
+end
+
+function serialize_to_file(data, file, uglify)
+  file = io.open(file, 'w+')
+  local serialized
+  if not uglify then
+    serialized = serpent.block(data, {
+        comment = false,
+        name = '_'
+      })
+  else
+    serialized = serpent.dump(data)
+  end
+  file:write(serialized)
+  file:close()
+end
+
+function is_sudo(msg)
+  local var = false
+  -- Check users id in config
+  for v,user in pairs(_config.sudo_users) do
+    if user == msg.from.id then
+      var = true
+    end
+  end
+  return var
+end
+
+function check_markdown(text) --markdown escape ( when you need to escape markdown , use it like : check_markdown('your text')
+		str = text
+		if str:match('_') then
+			output = str:gsub('_',[[\_]])
+		elseif str:match('*') then
+			output = str:gsub('*','\\*')
+		elseif str:match('`') then
+			output = str:gsub('`','\\`')
+		else
+			output = str
+		end
+	return output
+end

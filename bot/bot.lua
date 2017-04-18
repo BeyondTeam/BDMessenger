c6dbdde7809cd6572140c1ef1f2d552414ae592a
SoLiD <solid.in89@yahoo.com>
2017-04-19 04:17:39 +0430

    Beyond Messenger V1

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,6 +1,6 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-    bot_token = "231281320:AAH6F4ZhVWUDH3jHgLVEBWyJNaNxNUds6aE" --Put You Token Here
+    bot_token = "Token" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
 sudo_id = 157059515
 require('./bot/methods')

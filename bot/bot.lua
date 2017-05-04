f3c953084303107ca6c495f9b75041b327322132
SoLiD <solid.in89@yahoo.com>
2017-05-04 15:00:28 +0430

    Beyond Messenger V2

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,6 +1,6 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-    bot_token = "Token" --Put You Token Here
+    bot_token = "335796923:AAEbu3TKACG9R0jJqMEBen4ndjqkiguQna8" --Put You Token Here
    send_api = "https://api.telegram.org/bot"..bot_token
 sudo_id = 157059515
 require('./bot/methods')
@@ -140,6 +140,8 @@ function create_config( )
 *》Founder & Developer :* [SoLiD](Telegram.Me/SoLiD)
 _》Developer & Sponser :_ [MAKAN](Telegram.Me/MAKAN)
 _》Developer :_ [ToOfan](Telegram.Me/ToOfan)
+_》Developer :_ [Mohammad](Telegram.Me/Mohammadrezajiji)
+_》Developer :_ [AmirHossein](Telegram.Me/Sudo1)
 _》Developer :_ [Ehsan](Telegram.Me/CliFather)
 
 *》Special thanks to :*
